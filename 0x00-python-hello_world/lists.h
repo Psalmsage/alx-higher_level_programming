bfc9567b3fd643ab7747797b941e27b0366cac50
Psalm <Olonijayesamson@gmail.com>
2023-01-02 16:15:49 -0800

    learning more in python and c language so excited

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -16,8 +16,8 @@ typedef struct listint_s
 	int n;
 
 	struct listint_s *next;
-}
-listint _t;
+}listint _t;
+
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
