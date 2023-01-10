eb968196379cee25a6b85cf89d75a98e5998357c
Psalmsage <Olonijayesamson>
2023-01-10 18:14:29 +0000

    Python modules in Alx

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
 	from calculator_1 import add, sub ,mul, div
 
 	a = 10
-	b =  5
+	b = 5
 
 	print ("{} + {} = {}".format(a, b, add(a, b)))
 	print ("{} - {} = {}".format(a, b, sub(a, b)))
